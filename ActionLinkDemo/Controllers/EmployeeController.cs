77723919325d1c7eceeef0681f4231527c9ed732
Dương Dương <43409498+DuongLuke@users.noreply.github.com>
2024-10-11 14:08:23 +0700

    Update EmployeeController.cs

--- ActionLinkDemo/Controllers/EmployeeController.cs
+++ ActionLinkDemo/Controllers/EmployeeController.cs
@@ -1,1 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVC.Models;
 
+namespace WebApplicationMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee s=new Employee();
+          s.EmpId = 11;
+      s.EmpName = "Nguyen Thien Duong";
+      s.EmpDept = 32;
+      ViewBag.Employee = s;    
+    return View();
+            
+            return View();
+        }
+
+
+
+    }
+}
