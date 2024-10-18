4990e67f57590ddf5ea42a1e7ceec535965dac97
Dương Dương <43409498+DuongLuke@users.noreply.github.com>
2024-10-18 14:40:19 +0700

    Update HomeController.cs

--- ValidationMessageDemo/Controllers/HomeController.cs
+++ ValidationMessageDemo/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -18,39 +18,59 @@ namespace ValidationMessageDemo.Controllers
         public ActionResult Index(string Name,string Age, string PhoneNumber, string Email, string Password, string ConfirmPassword, string Gender, string Country, string Note, bool AcceptTerm)
         {
             if (Name.Equals("") == true)
+            {
                 ModelState.AddModelError("Name", "Name is required !");
                 ViewData["NameError"] = "*";
+            }
             if (Age.Equals("") == true)
+            {
                 ModelState.AddModelError("Age", "Age is required !");
                 ViewData["AgeError"] = "*";
+            }
             if (PhoneNumber.Equals("") == true)
+            {
                 ModelState.AddModelError("PhoneNumber", "PhoneNumber is required !");
                 ViewData["PhoneNumberError"] = "*";
+            }
             if (Email.Equals("") == true)
+            {
                 ModelState.AddModelError("Email", "Email is required !");
                 ViewData["EmailError"] = "*";
+            }
             if (Password.Equals("") == true)
+            {
                 ModelState.AddModelError("Password", "Password is required !");
                 ViewData["PasswordError"] = "*";
+            }
             if (ConfirmPassword.Equals("") == true)
+            {
                 ModelState.AddModelError("ConfirmPassword", "ConfirmPassword is required !");
                 ViewData["ConfirmPasswordError"] = "*";
+            }
             if (string.IsNullOrWhiteSpace(Gender))
+            {
                 ModelState.AddModelError("Gender", "Gender is required !");
                 ViewData["GenderError"] = "*";
+            }
             if (Country.Equals("") == true)
+            {
                 ModelState.AddModelError("Country", "Country is required !");
                 ViewData["CountryError"] = "*";
+            }
 
 
             if (Note.Equals("") == true)
+            {
                 ModelState.AddModelError("Note", "Note is required !");
                 ViewData["NoteError"] = "*";
-            
-               
+            }
+
+
             if (AcceptTerm != true)
+            {
                 ModelState.AddModelError("AcceptTerm", "AcceptTerm is required !");
                 ViewData["AcceptTermError"] = "*";
+            }
             if (ModelState.IsValid == true)
             {
                 ViewData["SuccessMessage"] = "<script>alert('Data has been Submitted successfully !')</script>";   
